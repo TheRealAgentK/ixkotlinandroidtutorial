52c061bf08ae86f0b209b319a8d96fb698ec90b8
Kai Koenig <kai@ventego-creative.co.nz>
2016-07-31 14:29:44 +1200

    Changes for Butter Knife view bindings

--- app/src/main/kotlin/nz/co/ventego_creative/ixkotlinandroidtutorial/MainActivity.kt
+++ app/src/main/kotlin/nz/co/ventego_creative/ixkotlinandroidtutorial/MainActivity.kt
@@ -2,11 +2,21 @@ package nz.co.ventego_creative.ixkotlinandroidtutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.helloWorld)
+    lateinit var helloWorld: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ButterKnife.bind(this)
+
+        helloWorld.text = "Hello from iX"
     }
 }
