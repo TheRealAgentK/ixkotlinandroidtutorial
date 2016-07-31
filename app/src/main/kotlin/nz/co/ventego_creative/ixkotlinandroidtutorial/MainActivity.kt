11d371dfdeb6cfc20ba172817a288f87753383cb
Kai Koenig <kai@ventego-creative.co.nz>
2016-07-31 18:04:51 +1200

    Added Button to Layout and Butter Knife-style click handler in Kotlin

--- app/src/main/kotlin/nz/co/ventego_creative/ixkotlinandroidtutorial/MainActivity.kt
+++ app/src/main/kotlin/nz/co/ventego_creative/ixkotlinandroidtutorial/MainActivity.kt
@@ -2,14 +2,21 @@ package nz.co.ventego_creative.ixkotlinandroidtutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
 
     @BindView(R.id.helloWorld)
     lateinit var helloWorld: TextView
+
+    @OnClick(R.id.clickMe)
+    fun clickedClickMe() {
+        Log.d("MainActivity", "onClick: clickMe")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +25,8 @@ class MainActivity : AppCompatActivity() {
         ButterKnife.bind(this)
 
         helloWorld.text = "Hello from iX"
+
     }
+
+
 }
